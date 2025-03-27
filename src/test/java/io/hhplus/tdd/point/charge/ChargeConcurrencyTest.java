5e6fd6aa67826fcff779c5a125f26f592f3e1bba
eunkang-choi <dmsrkd1216@gmail.com>
2025-03-28 00:21:07 +0900

    Test: 서로다른유저 동시성제어 테스트 추가

--- src/test/java/io/hhplus/tdd/point/charge/ChargeConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/point/charge/ChargeConcurrencyTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,6 +52,8 @@ public class ChargeConcurrencyTest {
 
 	@Autowired
 	private UserPointLockManager userPointLockManager;
+
+	private static final Logger log = LoggerFactory.getLogger(ChargeConcurrencyTest.class);
 
 
 	@BeforeEach
@@ -113,6 +117,61 @@ public class ChargeConcurrencyTest {
 		// then
 		long expectedPoint = chargeAmount * threadCount;
 		long finalPoint = userPointRepository.findById(id).point();
-		assertEquals(expectedPoint, finalPoint);
+		assertEquals(expectedPoint, finalPoint, "충전후 포인트값이 예상값("+expectedPoint+")과 실제값("+finalPoint+")이 서로다릅니다.");
+	}
+
+	@Test
+	void 서로_다른유저10명이_충전요청했을때_정상적으로_합산에_성공한다() throws Exception {
+		// given
+		int numberOfUsers = 10; // 인원수
+		int requestPerUser = 10; // 한사람당 요청개수
+
+		// 포인트 초기화
+		for(long id = 1L; id < numberOfUsers ; id++) {
+			userPointRepository.save(id, 0L);
+		}
+
+		ExecutorService executor = Executors.newFixedThreadPool(5); // 스레드풀: 5개. (요청 5개 동시에 실행가능)
+		CountDownLatch latch = new CountDownLatch(numberOfUsers * requestPerUser); // 10 * 10
+		long chargeAmount = 1000L;
+
+		// when
+		for(long id = 1L; id <= numberOfUsers; id++) {
+			ChargeRequestBody requestBody = new ChargeRequestBody(chargeAmount);
+			String json = objectMapper.writeValueAsString(requestBody);
+
+			// 유저 1명당 10번을 요청한다.
+			for(int i = 0 ; i < requestPerUser; i++) {
+				long uid = id;
+				executor.submit(() -> {
+					try {
+						mockMvc.perform(patch("/point/"+ uid +"/charge")
+							.contentType(MediaType.APPLICATION_JSON)
+							.content(json));
+					} catch(Exception e) {
+						e.printStackTrace();
+						throw new RuntimeException(e);
+					} finally {
+						latch.countDown();
+					}
+				});
+			}
+		}
+		long endTime = System.currentTimeMillis(); // 테스트종료 시각
+
+		latch.await();
+		Thread.sleep(1000);
+
+		// then
+		log.info("::: 테스트 종료후 유저별 보유포인트 출련 :::");
+		for(long id = 1L; id <= numberOfUsers; id++){
+			long actualPoint = userPointRepository.findById(id).point();
+			log.info("유저 ID {} 의 보유포인트: {}", id, actualPoint);
+		}
+
+		for(long id =1L; id <= numberOfUsers; id++) {
+			long actualPoint = userPointRepository.findById(id).point();
+			assertEquals(requestPerUser * chargeAmount, actualPoint,"UserPoint id "+id+ "인 회원의 포인트는 예상금액과 다릅니다: "+ actualPoint + "원" );
+		}
 	}
 }
